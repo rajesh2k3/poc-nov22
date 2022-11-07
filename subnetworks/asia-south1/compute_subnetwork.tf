e9b13202c6b3019e3d7aa3f30a981f447c9f580f
rajesh2k3 <47411556+rajesh2k3@users.noreply.github.com>
2022-11-07 13:15:14 +0530

    Update compute_subnetwork.tf

--- subnetworks/asia-south1/compute_subnetwork.tf
+++ subnetworks/asia-south1/compute_subnetwork.tf
@@ -1,7 +1,7 @@
-resource "google_compute_subnetwork" "tfer--default" {
+resource "google_compute_subnetwork" "ng-subnet" {
   ip_cidr_range              = "10.160.0.0/20"
-  name                       = "default"
-  network                    = "${data.terraform_remote_state.networks.outputs.google_compute_network_tfer--default_self_link}"
+  name                       = "ng-subnet"
+  network                    = "${data.terraform_remote_state.networks.outputs.google_compute_network_ng-subnet_self_link}"
   private_ip_google_access   = "true"
   private_ipv6_google_access = "DISABLE_GOOGLE_ACCESS"
   project                    = "poc-project-367814"
@@ -21,11 +21,11 @@ resource "google_compute_subnetwork" "tfer--default" {
   stack_type = "IPV4_ONLY"
 }
 
-resource "google_compute_subnetwork" "tfer--raj-subnet" {
+resource "google_compute_subnetwork" "raj-subnet" {
   description                = "raj-subnet"
   ip_cidr_range              = "10.0.2.0/24"
   name                       = "raj-subnet"
-  network                    = "${data.terraform_remote_state.networks.outputs.google_compute_network_tfer--raj-nw_self_link}"
+  network                    = "${data.terraform_remote_state.networks.outputs.google_compute_network_raj-nw_self_link}"
   private_ip_google_access   = "false"
   private_ipv6_google_access = "DISABLE_GOOGLE_ACCESS"
   project                    = "poc-project-367814"
