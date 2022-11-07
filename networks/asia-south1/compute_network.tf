35c7a1086d594cc18057e35f08c2c46012951b59
rajesh2k3 <47411556+rajesh2k3@users.noreply.github.com>
2022-11-07 13:08:10 +0530

    Update compute_network.tf

--- networks/asia-south1/compute_network.tf
+++ networks/asia-south1/compute_network.tf
@@ -1,15 +1,15 @@
-resource "google_compute_network" "tfer--default" {
+resource "google_compute_network" "ng-network" {
   auto_create_subnetworks         = "true"
   delete_default_routes_on_create = "false"
-  description                     = "Default network for the project"
+  description                     = "Network for the project"
   enable_ula_internal_ipv6        = "false"
-  mtu                             = "0"
-  name                            = "default"
+  mtu                             = "1500"
+  name                            = "ng-network"
   project                         = "poc-project-367814"
   routing_mode                    = "REGIONAL"
 }
 
-resource "google_compute_network" "tfer--raj-nw" {
+resource "google_compute_network" "raj-nw" {
   auto_create_subnetworks         = "false"
   delete_default_routes_on_create = "false"
   description                     = "raj-nw"
