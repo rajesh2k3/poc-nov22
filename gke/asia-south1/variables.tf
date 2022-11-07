d5dff656bd9abcc2d4a03c8084893a228ab13c16
rajesh2k3 <47411556+rajesh2k3@users.noreply.github.com>
2022-11-07 14:08:37 +0530

    Update variables.tf

--- gke/asia-south1/variables.tf
+++ gke/asia-south1/variables.tf
@@ -2,7 +2,8 @@ data "terraform_remote_state" "networks" {
   backend = "local"
 
   config = {
-    path = "../../../../../generated/google/poc-project-367814/networks/asia-south1/terraform.tfstate"
+    #path = "../../../../../generated/google/poc-project-367814/networks/asia-south1/terraform.tfstate"
+    path = "../../../poc-project-367814/networks/asia-south1/terraform.tfstate"
   }
 }
 
@@ -10,6 +11,8 @@ data "terraform_remote_state" "subnetworks" {
   backend = "local"
 
   config = {
-    path = "../../../../../generated/google/poc-project-367814/subnetworks/asia-south1/terraform.tfstate"
+    #path = "../../../../../generated/google/poc-project-367814/subnetworks/asia-south1/terraform.tfstate"
+    path = "../../../poc-project-367814/subnetworks/asia-south1/terraform.tfstate"
+    
   }
 }
