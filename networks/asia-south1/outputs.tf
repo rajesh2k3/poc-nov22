e624669e474e35ab8c94c41305f6e28d7aa9ab12
rajesh2k3 <47411556+rajesh2k3@users.noreply.github.com>
2022-11-07 13:09:17 +0530

    Update outputs.tf

--- networks/asia-south1/outputs.tf
+++ networks/asia-south1/outputs.tf
@@ -1,7 +1,7 @@
-output "google_compute_network_tfer--default_self_link" {
-  value = "${google_compute_network.tfer--default.self_link}"
+output "google_compute_network_ng-network_self_link" {
+  value = "${google_compute_network.ng-network.self_link}"
 }
 
-output "google_compute_network_tfer--raj-nw_self_link" {
-  value = "${google_compute_network.tfer--raj-nw.self_link}"
+output "google_compute_network_raj-nw_self_link" {
+  value = "${google_compute_network.raj-nw.self_link}"
 }
