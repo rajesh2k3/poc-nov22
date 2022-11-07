729f85941990f87fdb78eb013455ac49b4117e56
rajesh2k3 <47411556+rajesh2k3@users.noreply.github.com>
2022-11-07 13:16:25 +0530

    Update outputs.tf

--- subnetworks/asia-south1/outputs.tf
+++ subnetworks/asia-south1/outputs.tf
@@ -1,7 +1,7 @@
-output "google_compute_subnetwork_tfer--default_self_link" {
-  value = "${google_compute_subnetwork.tfer--default.self_link}"
+output "google_compute_subnetwork_ng-subnet_self_link" {
+  value = "${google_compute_subnetwork.ng-subnet.self_link}"
 }
 
-output "google_compute_subnetwork_tfer--raj-subnet_self_link" {
-  value = "${google_compute_subnetwork.tfer--raj-subnet.self_link}"
+output "google_compute_subnetwork_raj-subnet_self_link" {
+  value = "${google_compute_subnetwork.raj-subnet.self_link}"
 }
