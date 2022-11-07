ccee5ebd67bba718cc8cb82796329bcb1957d4b4
rajesh2k3 <47411556+rajesh2k3@users.noreply.github.com>
2022-11-07 12:59:05 +0530

    Update outputs.tf

--- gke/asia-south1/outputs.tf
+++ gke/asia-south1/outputs.tf
@@ -1,7 +1,7 @@
-output "google_container_cluster_tfer--nginx-cluster_self_link" {
-  value = "${google_container_cluster.tfer--nginx-cluster.self_link}"
+output "google_container_cluster_nginx-cluster_self_link" {
+  value = "${google_container_cluster.nginx-cluster.self_link}"
 }
 
-output "google_container_node_pool_tfer--nginx-cluster_default-pool_id" {
-  value = "${google_container_node_pool.tfer--nginx-cluster_default-pool.id}"
+output "google_container_node_pool_nginx-cluster_node-pool_id" {
+  value = "${google_container_node_pool.nginx-cluster_node-pool.id}"
 }
